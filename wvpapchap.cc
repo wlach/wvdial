8ac9aee4787422421ba6364f4e60b75f15e5e8f1
dcoombs <dcoombs@6619c942-59fa-0310-bf43-a07aaa81d9ac>
1998-11-18 19:43:39 +0000

    fixed a bug in papchap.  WvString related...
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@51 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- wvpapchap.cc
+++ wvpapchap.cc
@@ -69,6 +69,7 @@ bool WvPapChap::load_file( char * filename )
     from_file = file.getline( 0 );
     while( from_file ) {
     	tmp = new WvString( from_file );
+    	tmp->unique();
     	contents.append( tmp, true );
     	from_file = file.getline( 0 );
     }
