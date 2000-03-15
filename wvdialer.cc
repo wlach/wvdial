aba4097d9e7e33a3472d0d1e5642745f167afe01
apenwarr <apenwarr@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2000-03-15 20:44:55 +0000

    Updated copyright notices to "1997-2000" and "Net Integration Technologies, Inc."  It still says "Worldvisions Weaver software", though, because that's really what it is.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@185 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- wvdialer.cc
+++ wvdialer.cc
@@ -1,6 +1,6 @@
 /*
  * Worldvisions Weaver Software:
- *   Copyright (C) 1997, 1998, 1999 Worldvisions Computer Technology, Inc.
+ *   Copyright (C) 1997-2000 Net Integration Technologies, Inc.
  *
  * Implementation of the WvDialer smart-dialer class.  
  *
--- wvpapchap.h
+++ wvpapchap.h
@@ -1,6 +1,6 @@
 /*
  * Worldvisions Weaver Software:
- *   Copyright (C) 1997, 1998, 1999 Worldvisions Computer Technology, Inc.
+ *   Copyright (C) 1997-2000 Net Integration Technologies, Inc.
  *
  * Re-write of wvpapsecrets.h.  This one supports CHAP as well, and is also
  * much safer.
