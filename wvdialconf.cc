3f82fb988bd215a389e9b7d5b4758bbdfd472a76
apenwarr <apenwarr@6619c942-59fa-0310-bf43-a07aaa81d9ac>
2002-01-02 02:42:32 +0000

    Updated copyrights to 2002.
    git-svn-id: file:///home/apenwarr/alumnit-svn/public/trunk@382 6619c942-59fa-0310-bf43-a07aaa81d9ac

--- wvdialconf.cc
+++ wvdialconf.cc
@@ -1,6 +1,6 @@
 /*
  * Worldvisions Weaver Software:
- *   Copyright (C) 1997-2001 Net Integration Technologies, Inc.
+ *   Copyright (C) 1997-2002 Net Integration Technologies, Inc.
  *
  * WvDial configuration utility.  Generates a basic wvdial.conf file.
  */
